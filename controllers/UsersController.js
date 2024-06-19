9ee96d88f51401cec4e38020a75d7e003cabd330
ZiadHamdyy <ziadhamdy667@gmail.com>
2024-06-19 16:05:14 +0300

    task 3

---
+++ controllers/UsersController.js
@@ -0,0 +1,28 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.users.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const newUser = await dbClient.users.insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: newUser.insertedId, email });
+  }
+}
+
+export default UsersController;
